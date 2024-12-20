e8bfd2253d8d1a284baa960b99fac521af4249cf
Preeti Sharma <preetisharma0123@gmail.com>
2024-12-19 20:57:46 -0800

    fix: restructured dto

--- packages/server/src/users/auth/auth.controller.ts
+++ packages/server/src/users/auth/auth.controller.ts
@@ -14,7 +14,9 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto, AuthResponseDto } from '../dto';
-import { Public, GetUser, AuthUser } from '../../common/decorators';
+import { Public } from '../../common/decorators';
+import { AuthUser } from './decorators';
+import { GetUser } from '../decorators/get-user.decorator';
 import { RefreshTokenGuard, GoogleAuthGuard } from './guards';
 import { AuthResponse, Tokens, GoogleAuthRequest } from '../interfaces';
 
