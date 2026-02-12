3359522faa3eacb85dbb6ea194270b6d3b389d67
CeeJayyy007 <jamesjamil007@gmail.com>
2026-02-12 01:10:03 -0500

    chore: update google callback url

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -65,7 +65,7 @@ const SignUp = () => {
   };
 
   const handleGoogleSignin = () => {
-    window.open(`${config.api.baseURL}/api/auth/google`, "_self");
+    window.open(`${config.api.baseURL}/auth/google`, "_self");
   };
 
   const toggleShowPassword = () => setShowPassword(!showPassword);
