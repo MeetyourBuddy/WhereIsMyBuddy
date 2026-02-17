31fcb77ba8482a3867b2be5e03dc322b7bb49a02
CeeJayyy007 <jamesjamil007@gmail.com>
2026-02-17 00:55:13 -0500

    chore: fix google auth issues

--- packages/server/src/users/auth/strategies/google.strategy.ts
+++ packages/server/src/users/auth/strategies/google.strategy.ts
@@ -10,7 +10,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
-      callbackURL: `${configService.get('BACKEND_URL')}/auth/google/callback`,
+      callbackURL: `${configService.get('BACKEND_URL')}/api/auth/google/callback`,
       scope: ['email', 'profile'],
       prompt: 'select_account',
     });
