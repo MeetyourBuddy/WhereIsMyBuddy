444467a6e1afe442d7cfe3f8b4dea90fdc66c105
CeeJayyy007 <jamesjamil007@gmail.com>
2026-04-05 07:05:28 -0400

    chore: update cors origin

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
       'http://localhost:8080',
       'http://localhost:3000',
       'http://localhost:5173', // Vite default port
+      'http://wmb.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean), // Remove undefined values
     credentials: true,
